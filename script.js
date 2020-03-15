b88aa85eaa311ef31739073b0020a623548a9085
volha <v.khviadchenia@gmail.com>
2020-03-15 07:22:12 +0100

    feat: realize switching among tags

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ MENU.addEventListener('click', (event) => {
   MENU.querySelectorAll(".header-link").forEach(el => el.classList.remove('active'));
   event.target.classList.add('active');
   });
-  
+
+const PORTFOLIO = document.getElementById("portfolio")
+PORTFOLIO.addEventListener('click', (event) => {
+  PORTFOLIO.querySelectorAll(".portfolio-link").forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+    });

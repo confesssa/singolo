a1ba78e0a41d02b37df7cc2777d29846eb8b90cf
volha <v.khviadchenia@gmail.com>
2020-03-20 21:28:29 +0100

    feat: realize image-shift

--- script.js
+++ script.js
@@ -1,37 +1,64 @@
 const MENU = document.getElementById('menu');
 const TAGS = document.getElementById("tags")
 const LAYOUT = document.getElementById("layout");
+const MESSAGE = document.getElementById('message-block');
+const NAME = document.getElementById('name');
+const MAIL = document.getElementById('mail');
+const SUBJECT = document.getElementById('subject');
+const DESCRIPTION = document.getElementById('description');
 const BUTTON = document.getElementById('btn');
 const CLOSE_BUTTON = document.getElementById('close-btn');
 
 
+// MENU-SWITCHING
 MENU.addEventListener('click', (event) => {
   MENU.querySelectorAll(".header-link").forEach(el => el.classList.remove('active'));
   event.target.classList.add('active');
   });
 
-
+// TAG-SWITCHING
 TAGS.addEventListener('click', (event) => {
   TAGS.querySelectorAll(".portfolio-link").forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
+
+    let element = document.querySelectorAll('.portfolio-items > .portfolio-item:first-child');
+    document.portfolio-items.appendChild(element);
     });
 
-
+// IMAGE-SELECTION
 LAYOUT.addEventListener('click', (event) => {
   LAYOUT.querySelectorAll(".portfolio-image").forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
     });
 
-    
+// POPUP-SHOW
 BUTTON.addEventListener('click', () => {
-  const subject = document.getElementById('subject').value.toString();
-  document.getElementById('topic').innerText = subject;
-  document.getElementById('description').innerText = subject;
+  const topic = document.getElementById('topic').value.toString();
+  const description = document.getElementById('description').value.toString();
+  document.getElementById('topic').innerText = topic;
+  document.getElementById('description').innerText = description;
   document.getElementById('message-block').classList.remove('hidden');
+
+  if(topic !== '') {
+    document.getElementById('result-topic').innerText = topic;
+  } else {
+    document.getElementById('result-topic').innerText = 'Без темы'
+  }
+
+  if(description !== '') {
+    document.getElementById('result-description').innerText = description;
+  } else {
+    document.getElementById('result-description').innerText = 'Без описания';
+  }
+
 })
-    
+
+// POPUP-HIDDEN
 CLOSE_BUTTON.addEventListener('click', () => {
-  const subject = document.getElementById('subject').value.toString();
+  const topic = document.getElementById('topic').value.toString();
+  const description = document.getElementById('description').value.toString();
+  document.getElementById('name').innerText = '';
+  document.getElementById('mail').innerText = '';
   document.getElementById('topic').innerText = '';
   document.getElementById('description').innerText = '';
   document.getElementById('message-block').classList.add('hidden');
